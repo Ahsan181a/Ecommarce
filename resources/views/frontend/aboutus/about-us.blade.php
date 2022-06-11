5c1187e092830242da9f61458bb3220290396259
Ahsan181a <ahsanbondhan181@gmail.com>
2022-06-12 00:22:26 +0600

    about us

--- resources/views/frontend/aboutus/about-us.blade.php
+++ resources/views/frontend/aboutus/about-us.blade.php
@@ -3,106 +3,33 @@
     <!-- Title page -->
     <section class="bg-img1 txt-center p-lr-15 p-tb-92" id="abc" style="background-image:url('/frontend/images/bg-02.jpg');">
          @php
-            $contacts = DB::table('contacts')->first();    
+            $abouts = DB::table('abouts')->first();    
         @endphp
        
         <h2 class="ltext-105 cl0 txt-center">
-            {{$contacts->email}}
+            {{$abouts->title}}
         </h2>
         
     </section>  
 
     <!-- Content page -->
-    <section class="bg0 p-t-104 p-b-116">
-        <div class="container">
-            <div class="flex-w flex-tr">
-                <div class="size-210 bor10 p-lr-70 p-t-55 p-b-70 p-lr-15-lg w-full-md">
-                    <form method="post" action="" id="form">
-                        <h4 class="mtext-105 cl2 txt-center p-b-30">
-                            Send Us A Message
-                        </h4>
-                        <div class="bor8 m-b-20 how-pos4-parent">
-                            <input class="stext-111 cl2 plh3 size-116 p-l-62 p-r-30" type="text" name="name" placeholder="Your Name" value="" required="">
-                            <img class="how-pos4 pointer-none" src="{{asset('frontend/images/icons/user.png')}}" alt="ICON">
-                            <font color="red"><b></b></font>
-                        </div>
+    <section class="about-us py-5 " id="about-us">
+    <div class="container mt-5">
+    <div class="row">
+        <div class="col-md-6">
+            <h1 class='text-success'>Welcome!</h1>
+            <h2>Know More About Us</h2>
+            <hr>
+            <p>{{$abouts->description}}.</p>
+            <button type="button" class="btn btn-success">Let's Know More</button>
 
-                        <div class="bor8 m-b-20 how-pos4-parent">
-                            <input class="stext-111 cl2 plh3 size-116 p-l-62 p-r-30" type="email" name="email" placeholder="Your Email Address" value="" required="">
-                            <img class="how-pos4 pointer-none" src="{{asset('frontend/images/icons/icon-email.png')}}" alt="ICON">
-                            <font color="red"><b></b></font>
-                        </div>
-
-                        <div class="bor8 m-b-20 how-pos4-parent">
-                            <input class="stext-111 cl2 plh3 size-116 p-l-62 p-r-30" type="text" name="mobile" placeholder="Your Mobile Number" value="" required="">
-                            <img class="how-pos4 pointer-none" src="{{asset('frontend/images/icons/mobile.png')}}" alt="ICON">
-                            <font color="red"><b></b></font>
-                        </div>
-
-                        <div class="bor8 m-b-30">
-                            <textarea class="stext-111 cl2 plh3 size-120 p-lr-28 p-tb-25" name="msg" placeholder="Your Message" required=""></textarea>
-                            <font color="red"><b></b></font>
-                        </div>
-
-                        <button type="submit" class="flex-c-m stext-101 cl2 size-119 bg8 bor13 hov-btn3 p-lr-15 trans-04 pointer m-tb-10">
-                            Submit
-                        </button>
-                    </form>
-                </div>
-
-                <div class="size-210 bor10 flex-w flex-col-m p-lr-93 p-tb-30 p-lr-15-lg w-full-md">
-                    <div class="flex-w w-full p-b-42">
-                        <span class="fs-18 cl5 txt-center size-211">
-                            <span class="lnr lnr-map-marker"></span>
-                        </span>
-
-                        <div class="size-212 p-t-2">
-                            <span class="mtext-110 cl2">
-                                Address
-                            </span>
-
-                            <p class="stext-115 cl6 size-213 p-t-18">
-                                Notun bazar,Gulshan-Dhaka
-                            </p>
-                        </div>
-                    </div>
-
-                    <div class="flex-w w-full p-b-42">
-                        <span class="fs-18 cl5 txt-center size-211">
-                            <span class="lnr lnr-phone-handset"></span>
-                        </span>
-
-                        <div class="size-212 p-t-2">
-                            <span class="mtext-110 cl2">
-                                Lets Talk
-                            </span>
-
-                            <p class="stext-115 cl1 size-213 p-t-18">
-                                +088 01928511049
-                            </p>
-                        </div>
-                    </div>
-
-                    <div class="flex-w w-full">
-                        <span class="fs-18 cl5 txt-center size-211">
-                            <span class="lnr lnr-envelope"></span>
-                        </span>
-
-                        <div class="size-212 p-t-2">
-                            <span class="mtext-110 cl2">
-                                Sale Support
-                            </span>
-
-                            <p class="stext-115 cl1 size-213 p-t-18">
-                                asadullahkpi@gmail.com
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
         </div>
-    </section>  
-    
+        <div class="col-md-6">
+            <img src="http://themebubble.com/demo/marketingpro/wp-content/uploads/2016/10/seo-slide.png "alt="">
+        </div>
+    </div>
+</div>
+</section>
     <!-- Map -->
     <div class="container">
         <div class="row">
